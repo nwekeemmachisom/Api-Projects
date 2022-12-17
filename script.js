773dfda4c45b838017ef816821352ead9d432ddd
Your Name <you@example.com>
2022-12-17 13:59:46 +0100

    My Api Project

---
+++ script.js
@@ -0,0 +1,49 @@
+let title = window.document.getElementById("title")
+let year = window.document.getElementById("year")
+let type = window.document.getElementById("type")
+let append = window.document.getElementById("append")
+let imgdiv = window.document.getElementById("imgdiv")
+let movies = [];
+let noOfMovies = 0;
+let yearcheck;
+let img;
+
+
+
+let butt = window.document.getElementById("butt")
+butt.addEventListener("click",()=>{
+    imgdiv.style.display = 'block'
+    console.log(title.value)
+    console.log(year.value)
+
+    fetch(`https://omdbapi.com/?apikey=9e2b4991&s=${title.value}&y=${year.value}&type=${type.value}`)
+    .then(async (res) => {
+        const response = await res.json();
+        movies = response.Search;
+        noOfMovies = response.totalResults;
+        console.log(movies)
+
+        movies.forEach(element => {
+            console.log(element.Title)
+            console.log(element.Year)
+            let create = document.createElement("section")
+            create.innerHTML = `<p class="container">
+            ${element.Title}<br>
+            ${element.Year}
+            </p>`
+
+            append.appendChild(create)
+            append.style.backgroundColor = 'green'
+        });
+     })
+
+    .catch((err) => {
+        console.log('error')
+        console.log(err)
+    })
+    .finally(() => 
+    imgdiv.style.display = 'none'
+)
+
+    
+})
