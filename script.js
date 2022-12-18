0b352b8c8e985b262022f92de85c6293e65cc5f8
Your Name <you@example.com>
2022-12-18 18:24:28 +0100

    modified api files

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let year = window.document.getElementById("year")
 let type = window.document.getElementById("type")
 let append = window.document.getElementById("append")
 let imgdiv = window.document.getElementById("imgdiv")
+let number = window.document.getElementById("noOfMovies")
 let movies = [];
 let noOfMovies = 0;
 let yearcheck;
@@ -23,18 +24,22 @@ butt.addEventListener("click",()=>{
         noOfMovies = response.totalResults;
         console.log(movies)
 
+        
+        
         movies.forEach(element => {
             console.log(element.Title)
             console.log(element.Year)
             let create = document.createElement("section")
-            create.innerHTML = `<p class="container">
+            create.innerHTML = `<p class="container pt-3 pb-3">
             ${element.Title}<br>
             ${element.Year}
+           
             </p>`
-
             append.appendChild(create)
-            append.style.backgroundColor = 'green'
+            append.style.backgroundColor = '#B3080F'
         });
+        number.innerHTML = `<p>Total results:<br>
+        ${noOfMovies}</p>`
      })
 
     .catch((err) => {
